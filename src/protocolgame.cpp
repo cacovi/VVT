5d6188d255dccb590ea97c179b8c5392beeee8da
cacovi <52906969+cacovi@users.noreply.github.com>
2020-05-23 03:32:05 -0400

    Revert "Revert "reload creature""
    This reverts commit 1ff54ef4babeea51d9aaac425d03434bacd9bf4f.

--- src/protocolgame.cpp
+++ src/protocolgame.cpp
@@ -3349,7 +3349,7 @@ void ProtocolGame::sendFightModes()
 	writeToOutputBuffer(msg);
 }
 
-void ProtocolGame::sendAddCreature(const Creature* creature, const Position& pos, int32_t stackpos, bool isLogin)
+void ProtocolGame::sendAddCreature(const Creature* creature, const Position& pos, int32_t stackpos, bool isLogin, std::string newName/* = ""*/)
 {
 	if (!canSee(pos)) {
 		return;
@@ -3367,7 +3367,10 @@ void ProtocolGame::sendAddCreature(const Creature* creature, const Position& pos
 		bool known;
 		uint32_t removedKnown;
 		checkCreatureAsKnown(creature->getID(), known, removedKnown);
-		AddCreature(msg, creature, known, removedKnown);
+		if (newName != "") 
+			AddCreature(msg, creature, known, removedKnown, newName);
+		else
+			AddCreature(msg, creature, known, removedKnown);
 		writeToOutputBuffer(msg);
 
 		if (isLogin) {
@@ -4034,7 +4037,7 @@ void ProtocolGame::sendModalWindow(const ModalWindow& modalWindow)
 }
 
 ////////////// Add common messages
-void ProtocolGame::AddCreature(NetworkMessage& msg, const Creature* creature, bool known, uint32_t remove)
+void ProtocolGame::AddCreature(NetworkMessage& msg, const Creature* creature, bool known, uint32_t remove, std::string newName/* = ""*/)
 {
 	CreatureType_t creatureType = creature->getType();
 
@@ -4058,7 +4061,11 @@ void ProtocolGame::AddCreature(NetworkMessage& msg, const Creature* creature, bo
 			}
 		}
 
-		msg.addString(creature->getName());
+		std::string creature_name = creature->getName();
+			if (newName != "") {
+				creature_name = newName;
+			} else if (const Monster* monster = creature->getMonster()) {
+				const std::string newNameMonster = monster->getNewName();
 	}
 
 	if (creature->isHealthHidden()) {
@@ -4671,6 +4678,32 @@ void ProtocolGame::reloadCreature(const Creature* creature)
 		AddCreature(msg, creature, false, 0);
 	} else {
 		sendAddCreature(creature, creature->getPosition(), stackpos, false);
+	}
+
+	writeToOutputBuffer(msg);
+}
+
+void ProtocolGame::reloadCreature(const Creature* creature, const std::string newName)
+{
+	if (!canSee(creature))
+		return;
+
+	uint32_t stackpos = creature->getTile()->getClientIndexOfCreature(player, creature);
+
+	if (stackpos >= 10)
+		return;
+
+	NetworkMessage msg;
+
+	std::unordered_set<uint32_t>::iterator it = std::find(knownCreatureSet.begin(), knownCreatureSet.end(), creature->getID());
+	if(it != knownCreatureSet.end()) {
+		RemoveTileThing(msg, creature->getPosition(), stackpos);
+		msg.addByte(0x6A);
+		msg.addPosition(creature->getPosition());
+		msg.addByte(stackpos);
+		AddCreature(msg, creature, false, creature->getID(), newName);
+	} else {
+		sendAddCreature(creature, creature->getPosition(), stackpos, false, newName);
 	}
 
 	writeToOutputBuffer(msg);
